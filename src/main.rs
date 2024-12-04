3bae6a945c528128f3aa6b34de5a612936a4a3d6
farhan <farhanalirazaazeemi@gmail.com>
2024-12-04 18:31:28 +0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -16,77 +16,7 @@ fn main() {
 
     let mut  sum = 0;
 
-    let lines : Vec<&str> = contents.split("\n").collect();
-
   
-
-
-
-
-    let mut vov = [].to_vec();
-
-
-    for line in lines {
-        let voc: Vec<char> = line.chars().collect();
-        if voc.len() > 0 {
-            vov.push(voc);
-        }
-    }
-
-
-    println!("{:?}", vov);
-
-    let height = vov.len();
-    let width = vov[0].len();
-    
-    let mas = "MAS";
-    let sam = "SAM";
-    for y in 0..height {
-        for x in 0..width {
-
-            println!("{:?}", vov[y][x]);
-            if vov[y][x] == 'M' || vov[y][x] == 'S' {
-     
-
-                //  diagonal Down right
-
-                if y + 2 < height && x + 2 < width {
-                    let mut s = String::new();
-
-                    for i in 0..3 {
-                        s.push(vov[y+i][x+i]);
-                    }
-
-                    if s == sam || s == mas {
-                        println!("Digonal Down Right: {:?}", s);
-
-                        if y + 2 < height && x+2 > 1 {
-                            let mut s = String::new();
-        
-                            for i in 0..3 {
-                                s.push(vov[y+i][x+2-i]);
-                            }
-        
-                            if s == sam || s == mas  {
-                                println!("Digonal Down left: {:?}", s);
-                                sum += 1;
-                            }
-                        }
-
-                        
-                    }
-                }
-         
-
-
-
-            }
-
-        }
-
-    }
-
-
 
 
 
